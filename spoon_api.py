f0f1e77c5d2157de8cfb66f4f47f16628dad79e1
mayawhaley <mawhaley430@gmail.com>
2020-02-11 17:06:55 +0000

    Debug

--- spoon_api.py
+++ spoon_api.py
@@ -7,17 +7,15 @@ url = "https://api.spoonacular.com/recipes/complexSearch?query=pudding&addRecipe
 
 # API_Key = "541651421a774a08bbadc9089fe32d78"
 
-api_key = os.getenv("API_Key")
 
-# "541651421a774a08bbadc9089fe32d78"
+oauth = requests_oauthlib.OAuth1(
+    "API_Key"
+    )
 
-# oauth = requests_oauthlib.OAuth1(
-    
-#     )
-
-response = requests.get(url, auth=api_key)
-# ,auth=oauth)
+response = requests.get(url,auth=oauth)
 json_body = response.json()
+# print(json.dumps(json_body))
+
 
 
 
