714d508f8b6814446e421b72fecb9bfb4c3fa159
mayawhaley <mawhaley430@gmail.com>
2020-02-11 19:43:47 +0000

    Debug

--- twitter_api.py
+++ twitter_api.py
@@ -1,16 +1,22 @@
 import requests
 import requests_oauthlib
 import json
+import os
 
 url = "https://api.twitter.com/1.1/search/tweets.json?q=pudding%20dessert&tweet_mode=extended"
 
+key = os.getenv("TWI_API_Key")
+secret = os.getenv("TWI_API_Secret_Key")
+token = os.getenv("TWI_Access_Token")
+token_secret = os.getenv("TWI_Access_Token_Secret")
+
 oauth = requests_oauthlib.OAuth1(
-    "TWI_API_Key",
-    "TWI_API_Secret_Key",
-    "TWI_Access_Token",
-    "TWI_Access_Token_Secret"
+    key,
+    secret,
+    token,
+    token_secret
     )
 
-response = requests.get(url,auth=oauth)
+# response = requests.get(url,auth=oauth)
+response = requests.get(url, auth=oauth)
 json_body = response.json()
-print(json.dumps(json_body))
