f6cd4cb3ccb94ad69cd606e812a8286a915ad1c3
mayawhaley <mawhaley430@gmail.com>
2020-02-11 19:25:35 +0000

    Debug

--- twitter_api.py
+++ twitter_api.py
@@ -5,12 +5,10 @@ import json
 url = "https://api.twitter.com/1.1/search/tweets.json?q=pudding%20dessert&tweet_mode=extended"
 
 oauth = requests_oauthlib.OAuth1(
-    "gnyHPeOlmdYuU4ecionYCPjOt",
-    "clBi6W7KgQ9hoIQ9sZ3DbQAxOkp2k8ZGsUSe0F8ifQR7esp5Vs",
-    
-    
-    "258103267-FCvy1NDM1wXGVmHzYnEPtL6k25ThFGs2PlHJI9YW",
-    "w202ArZEDrQNSrY546wDCaPUtDed3R4So3s9yYhjgBhjL"
+    "TWI_API_Key"
+    "TWI_API_Secret_Key"
+    "TWI_Access_Toke"
+    "TWI_Access_Token_Secret"
     )
 
 response = requests.get(url,auth=oauth)
